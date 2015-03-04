46f822142d94186fed1ffcb0dd8b29cf0b62c479
Nikola Milivojevic <0dziga0@gmail.com>
2015-03-04 21:48:25 +0100

    title screen

--- js/resources.js
+++ js/resources.js
@@ -5,7 +5,6 @@ game.resources = [
     {name: "hive",                      type:"image", src: "data/img/sprite/hive.png"},
     {name: "hive-hint",                 type:"image", src: "data/img/sprite/hive-hint.png"},
     {name: "32x32_font",                type:"image", src: "data/img/font/32x32_font.png"},
-    {name: "maja_title_screen",         type:"image", src: "data/img/gui/maja.png"},
     {name: "background-img",            type:"image", src: "data/img/background-img.jpg"},
     {name: "play",                      type:"image", src: "data/img/gui/play.ico"},
     {name: "pause",                     type:"image", src: "data/img/gui/pause.ico"},
--- js/screens/title.js
+++ js/screens/title.js
@@ -6,22 +6,91 @@ game.TitleScreen = me.ScreenObject.extend({
       me.game.world.addChild(
         new me.Sprite (
           0,0,
-          me.loader.getImage('maja_title_screen')
+          me.loader.getImage('background-img')
         ),
         1
       );
 
-      this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
-        if (action === "click") {
+      me.game.world.addChild(new (me.GUI_Object.extend ({
+        init:function (x, y) {
+          var settings = {}
+          settings.image = "plus";
+          settings.spritewidth = 80;
+          settings.spriteheight = 80;
+          // super constructor
+          this._super(me.GUI_Object, "init", [100, 100, settings]);
+          // define the object z order
+          this.z = 4;
+        },
+
+        onClick:function (event) {
+          game.data.speed = 1;
+          game.data.hint = true;
+          game.data.arithmetic.allowed = ["+","-"];
           me.state.change(me.state.PLAY);
+          return false;
         }
-      });
+      })),2);
+
+      me.game.world.addChild(new (me.GUI_Object.extend ({
+        init:function (x, y) {
+          var settings = {}
+          settings.image = "minus";
+          settings.spritewidth = 80;
+          settings.spriteheight = 80;
+          // super constructor
+          this._super(me.GUI_Object, "init", [200, 100, settings]);
+          // define the object z order
+          this.z = 4;
+        },
+
+        onClick:function (event) {
+          me.state.change(me.state.PLAY);
+          return false;
+        }
+      })),3);
+
+      me.game.world.addChild(new (me.GUI_Object.extend ({
+        init:function (x, y) {
+          var settings = {}
+          settings.image = "multiply";
+          settings.spritewidth = 80;
+          settings.spriteheight = 80;
+          // super constructor
+          this._super(me.GUI_Object, "init", [100, 200, settings]);
+          // define the object z order
+          this.z = 4;
+        },
+
+        onClick:function (event) {
+          me.state.change(me.state.PLAY);
+          return false;
+        }
+      })),4);
+
+      me.game.world.addChild(new (me.GUI_Object.extend ({
+        init:function (x, y) {
+          var settings = {}
+          settings.image = "divide";
+          settings.spritewidth = 80;
+          settings.spriteheight = 80;
+          // super constructor
+          this._super(me.GUI_Object, "init", [200, 200, settings]);
+          // define the object z order
+          this.z = 4;
+        },
+
+        onClick:function (event) {
+          me.state.change(me.state.PLAY);
+          return false;
+        }
+      })),5);
     },
 
     /**
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
-      me.event.unsubscribe(this.handler);
+      // me.event.unsubscribe(this.handler);
     }
 });
